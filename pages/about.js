70abbcc7bc81dcdf95d750f4e3c917a531a0739b
Mohammed Nabeel km <nabeelkmvkd07@gmail.com>
2020-11-04 16:59:45 +0530

    initial commit , set index and about pages

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+const About = () => {
+    return (
+        <div>
+            <h1>Aboutus</h1>
+            <p>fddsfgqwo0qdosaid;dqw9ru0 ;qwd asncjasfga sgifabdad</p>
+        </div>
+    )
+}
+export default About;
